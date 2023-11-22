3e62dac42ad680a5981e4359d1234bd97d362552
ka269789 <141327314+ka269789@users.noreply.github.com>
2023-11-22 19:00:45 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,258 @@
+<?php
+$html = '
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>文博士答案</title>
+  <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+<div class="nav">
+
+  <div class="content">
+  </div>
+  <script src="script.js"></script>
+</body>
+</html>
+<script language=“javascript”>
+var noPrint=true;
+var noCopy=true;
+var noScreenshot=true;
+var autoBlur=true;
+</script>
+<script type=“text/javascript” src=“pdfanticopy.com_noprint.js”></script>
+<title>文博士答案</title>
+<link rel="icon" type="image/x-icon" href="favicon.ico" />
+<h1 align="center" id="wbsda">文博士答案</h1>
+<h4 align="center" id="sm">本站为免费网站不要截图！！！</h4>
+<h3>如果有作文点击按钮</h3>
+<form action="https://www.forchangeai.com/" method="get">
+    <input type="submit" value="ai帮写作文">
+</form>
+<h2>下载ai获取pay</h2>
+<a href="http://aigcfun.com/app-download" target="_blank">
+        <button>下载</button></a>
+<h1>如果你不想抄答案有聊天室讨论作业</h1>
+<form action="\lts\index.php" method="get">
+    <input type="submit" value="聊天室">
+</form>
+<h1>网站协议</h1>
+
+<html>
+<body>
+    <a href="xy\ine.html" target="_blank">
+        <button>查看</button>
+    </a>
+</body>
+</html>
+<h1>下载客户端</h1>
+<a href="/xz/in.html" target="_blank">
+        <button>下载</button>
+    </a>
+<h1>英语课文</h1>
+<form action="ykwmp3\yy.html" method="get">
+    <input type="submit" value="课文1-4单元">
+</form>
+<h1>公告网站</h1>
+<!DOCTYPE html>
+    <a href="/z-blog/" target="_blank">
+        <button>进入</button>
+    </a>
+<h1>英语：</h1>
+<img src="ying1.jpg"
+width="385"
+height="600"/>
+<img src="ying2.jpg"
+width="385"
+height="600"/>
+<img src="ying3.jpg"
+width="385"
+height="600"/>
+<img src="ying4.jpg"
+width="385"
+height="600"/>
+<img src="ying5.jpg"
+width="385"
+height="600"/>
+<h1>数学:</h1>
+    <a href="jsp.html" target="_blank">
+        <button>计算器</button>
+    </a>
+<img src="s1.jpg"
+width="385"
+height="600"/>
+<img src="s2.jpg"
+width="385"
+height="600"/>
+<img src="s3.jpg"
+width="385"
+height="600"/>
+<img src="s4.jpg"
+width="385"
+height="600"/>
+<img src="s5.jpg"
+width="385"
+height="600"/>
+<img src="s6.jpg"
+width="385"
+height="600"/>
+<img src="s7.jpg"
+width="385"
+height="600"/>
+<img src="s8.jpg"
+width="385"
+height="600"/>
+<img src="s9.jpg"
+width="385"
+height="600"/>
+<img src="s10.jpg"
+width="385"
+height="600"/>
+<img src="s11.jpg"
+width="385"
+height="600"/>
+<img src="s12.jpg"
+width="385"
+height="600"/>
+<h1>语文:</h1>
+<img src="y1.jpg"
+width="385"
+height="600"/>
+<img src="y2.jpg"
+width="385"
+height="600"/>
+<img src="y3.jpg"
+width="385"
+height="600"/>
+<img src="y4.jpg"
+width="385"
+height="600"/>
+<img src="y5.jpg"
+width="385"
+height="600"/>
+<img src="y6.jpg"
+width="385"
+height="600"/>
+<img src="y7.jpg"
+width="385"
+height="600"/>
+<img src="y8.jpg"
+width="385"
+height="600"/>
+<img src="y9.jpg"
+width="385"
+height="600"/>
+<img src="y10.jpg"
+width="385"
+height="600"/>
+<img src="y11.jpg"
+width="385"
+height="600"/>
+<img src="y12.jpg"
+width="385"
+height="600"/>
+<img src="y13.jpg"
+width="385"
+height="600"/>
+<img src="y14.jpg"
+width="385"
+height="600"/>
+<img src="y15.jpg"
+width="385"
+height="600"/>
+<img src="y16.jpg"
+width="385"
+height="600"/>
+<h1>本站提供音乐</h1>
+<audio controls="controls" loop="loop">
+<source src="4126299374.mp3" type="audio/mp3"></source>
+</audio>
+<audio controls="controls" loop="loop">
+<source src="f61bc6e0f1f9c651a92bfe616512f7a6.mp3" type="audio/mp3"></source>
+</audio>
+<audio controls="controls" loop="loop">
+<source src="3516448795.mp3" type="audio/mp3"></source>
+</audio>
+<audio controls="controls" loop="loop">
+<source src="775090310.mp3" type="audio/mp3"></source>
+</audio>
+</audio>
+<audio controls="controls" loop="loop">
+<source src="M800004QaHZ32xy1qh.MP3" type="audio/mp3"></source>
+</audio>
+<audio controls="controls" loop="loop">
+<source src="obj_wo3DlMOGwrbDjj7DisKw_23938658332_ebec_d86f_ad88_09a0263dee3d573cafe6e84a74bc5262.mp3" type="audio/mp3"></source>
+</audio>
+<h1>源代码</h1>
+<form action="ydm\index.html" method="get">
+    <input type="submit" value="源代码">
+<h4 style="text-align: left;">特别感谢李金*，李子*，网站作者是相**</h4>
+<h4 align="center" id="jg">本站为免费网站不要截图！！！</h4>
+<h4>网站框架:</h4>
+<img class="logo" src="htmlbz.jpg">
+<img class="php" src="phpbt.jpg">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<style>
+/*logos*/
+.logo{
+  width:20vw;
+  transform: translate(70px, -44.96px); /* 移动图片的X轴和Y轴 */
+}
+.php{
+  width:35vw;
+  transform: translate(70px, -44.96px); /* 移动图片的X轴和Y轴 */
+}
+/* 默认样式 */
+.container {
+  width: 100%;
+  max-width: 960px;
+  margin: 0 auto;
+  padding: 20px;
+  background-color: #f2f2f2;
+}
+/* 在较小的屏幕上应用不同的样式 */
+@media (max-width: 600px) {#87CEFA
+  .container {
+    padding: 10px;
+  }
+}
+/* 将所有按钮样式设置为圆角长方形 */
+    button, input[type="button"], input[type="submit"] {
+      border-radius: 8.83px;
+      padding: 11.48px 18.65px; /* 调整按钮内边距根据需要 */
+      background-color: #000;
+      color: white;
+      border: none;
+    }
+@font-face {
+  font-family: "bz"; /* 字体名称 */
+  src: url(zt/h1b.ttf) format("truetype"); /* 字体文件路径和格式 */
+}
+@font-face {
+  font-family: "h1bt"; /* 字体名称 */
+  src: url(zt/h1.ttf) format("truetype"); /* 字体文件路径和格式 */
+}
+
+#wbsda{
+  font-family: "bz", sans-serif; /* 使用自定义字体 "bz"，如果不存在，使用系统默认字体 */
+}
+#sm{
+  font-family: "bz", sans-serif; /* 使用自定义字体 "bz"，如果不存在，使用系统默认字体 */
+}
+h1 {
+  font-family: "h1bt", sans-serif; /* 使用自定义字体 "bz"，如果不存在，使用系统默认字体 */
+}
+h2 {
+  font-family: "h1bt", sans-serif; /* 使用自定义字体 "bz"，如果不存在，使用系统默认字体 */
+}
+h1 {
+  font-family: "h1bt", sans-serif; /* 使用自定义字体 "bz"，如果不存在，使用系统默认字体 */
+}
+
+</style>
+</html>
+';
+echo $html;
+?>
